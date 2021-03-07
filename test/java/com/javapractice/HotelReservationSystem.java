b16653da73660089a764dee57efe0c47c8984ac9
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-03-07 20:45:59 +0530

    Added : Displaying Welcome Message

---
+++ test/java/com/javapractice/HotelReservationSystem.java
@@ -0,0 +1,9 @@
+package com.javapractice;
+import java.util.Scanner;
+
+public class HotelReservationSystem {
+
+    public static void main(String[] args) {
+        System.out.println("<<< Welcome to Hotel Reservation System >>>");
+    }
+}
