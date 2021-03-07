814228891b7d917dcf5f2729b90b80b2fa6e7a1b
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-03-07 21:42:04 +0530

    Updated : Ablity to add weekday and weekend rate for each hotel

--- test/java/com/javapractice/HotelReservationSystem.java
+++ test/java/com/javapractice/HotelReservationSystem.java
@@ -9,35 +9,21 @@ public class HotelReservationSystem {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Hotel Name \t: ");
         String hotelName = sc.nextLine();
-        System.out.print("Enter Rate : \t");
-        int hotelRate = sc.nextInt();
+        System.out.print("Weekday Rate : \t");
+        int weekdayRate = sc.nextInt();
+        System.out.print("Weekend Rate : \t");
+        int weekendRate = sc.nextInt();
 
-        hotels.add(new Hotel(hotelName, hotelRate));
+        hotels.add(new Hotel(hotelName, new Integer[]{weekdayRate,weekendRate}));
         System.out.println("Hotel added successfully..!");
-    }
-    public void findCheapest(){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter date range.\nEnter Start Date : ");
-        String startDate = sc.nextLine();
-        System.out.print("Enter End Date : ");
-        String endDate = sc.nextLine();
-        int min = hotels.get(0).getRate();
-        int ratingIndex = 0;
-        for(int i=1;i<hotels.size();i++){
-            min = Math.min(min,hotels.get(i).getRate());
-            if(hotels.get(i).getRate() == min)
-                ratingIndex = i;
-        }
-        System.out.println("Best Cheapest Hotel : " + hotels.get(ratingIndex).getNameOfHotel());
-        System.out.println("Rate           : " + hotels.get(ratingIndex).getRate());
     }
 
     public void showHotels() {
         System.out.println("Hotels ->");
-        System.out.printf("%-20s%-20s\n","Name of hotel","Rate");
+        System.out.printf("%-20s%-20s%-20s\n","Name of hotel","WeekdayRate","WeekendRate");
         for(int i = 0 ;i<hotels.size();i++)
         {
-            System.out.printf("%-20s%-20d\n",hotels.get(i).getNameOfHotel(),hotels.get(i).getRate());
+            System.out.printf("%-20s%-20d%-20d\n",hotels.get(i).getNameOfHotel(),hotels.get(i).getRate()[0],hotels.get(i).getRate()[1]);
         }
     }
     public static void main(String[] args) {
@@ -49,15 +35,13 @@ public class HotelReservationSystem {
         while (true) {
             System.out.println("1. Add a New Hotel.");
             System.out.println("2. Show Available Hotels.");
-            System.out.println("3. Find Cheapest Available Hotels.");
-            System.out.println("4. Exit From Hotel Reservation System.");
+            System.out.println("3. Exit From Hotel Reservation System.");
             System.out.print("Enter your choice : ");
             choice = sc.nextInt();
             switch (choice) {
                 case 1: h.addHotel();break;
-                case 2:h.showHotels();break;
-                case 3 : h.findCheapest();break;
-                case 4: return;
+                case 2: h.showHotels();break;
+                case 3: return;
                 default:
                     System.out.println("Wrong choice.");
             }
