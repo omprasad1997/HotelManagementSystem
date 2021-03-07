b66eac0ebfc20b91fefae9cc94f2abc1b8c45663
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-03-07 21:17:08 +0530

    Updated : Ability to find best cheapest hotel for given date range

--- test/java/com/javapractice/HotelReservationSystem.java
+++ test/java/com/javapractice/HotelReservationSystem.java
@@ -15,6 +15,23 @@ public class HotelReservationSystem {
         hotels.add(new Hotel(hotelName, hotelRate));
         System.out.println("Hotel added successfully..!");
     }
+    public void findCheapest(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter date range.\nEnter Start Date : ");
+        String startDate = sc.nextLine();
+        System.out.print("Enter End Date : ");
+        String endDate = sc.nextLine();
+        int min = hotels.get(0).getRate();
+        int ratingIndex = 0;
+        for(int i=1;i<hotels.size();i++){
+            min = Math.min(min,hotels.get(i).getRate());
+            if(hotels.get(i).getRate() == min)
+                ratingIndex = i;
+        }
+        System.out.println("Best Cheapest Hotel : " + hotels.get(ratingIndex).getNameOfHotel());
+        System.out.println("Rate           : " + hotels.get(ratingIndex).getRate());
+    }
+
     public void showHotels() {
         System.out.println("Hotels ->");
         System.out.printf("%-20s%-20s\n","Name of hotel","Rate");
@@ -32,13 +49,15 @@ public class HotelReservationSystem {
         while (true) {
             System.out.println("1. Add a New Hotel.");
             System.out.println("2. Show Available Hotels.");
-            System.out.println("3. Exit From Hotel Reservation System.");
+            System.out.println("3. Find Cheapest Available Hotels.");
+            System.out.println("4. Exit From Hotel Reservation System.");
             System.out.print("Enter your choice : ");
             choice = sc.nextInt();
             switch (choice) {
                 case 1: h.addHotel();break;
                 case 2:h.showHotels();break;
-                case 3: return;
+                case 3 : h.findCheapest();break;
+                case 4: return;
                 default:
                     System.out.println("Wrong choice.");
             }
