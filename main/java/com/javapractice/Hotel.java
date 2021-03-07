5fd0938feae288e9250d094c6e1a8767b09f807f
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-03-07 21:06:23 +0530

    Updated : Ability to add hotel name and rate for regular customer

--- main/java/com/javapractice/Hotel.java
+++ main/java/com/javapractice/Hotel.java
@@ -1,8 +1,36 @@
 package com.javapractice;
 
 public class Hotel{
-    void welcome(){
-        System.out.println("<<< Welcome to Hotel Reservation System >>>");
+    private String nameOfHotel;
+    private int rate;
+
+    public Hotel(String nameOfHotel, int rate) {
+        this.nameOfHotel = nameOfHotel;
+        this.rate = rate;
+    }
+
+    public String getNameOfHotel() {
+        return nameOfHotel;
+    }
+
+    public void setNameOfHotel(String nameOfHotel) {
+        this.nameOfHotel = nameOfHotel;
+    }
+
+    public int getRate() {
+        return rate;
+    }
+
+    public void setRate(int rate) {
+        this.rate = rate;
+    }
+
+    @Override
+    public String toString() {
+        return "Hotel{" +
+                "nameOfHotel='" + nameOfHotel + '\'' +
+                ", rate=" + rate +
+                '}';
     }
 }
 
